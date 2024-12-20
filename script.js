5ed86460fbc2ef88c6c9f720fbd5f3f62e4b2414
alekhya <alekhyamadaka3@gmail.com>
2024-12-20 12:54:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript for handling form validation and other interactions (if necessary)
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Your message has been sent!');
+});
